44bde370cd4ed3e72d3ca6ecbed2a9084424e35a
seo <seotownsend@icloud.com>
2016-03-18 00:31:03 -0400

    wip

--- lib/plumbus/file_generators.rb
+++ lib/plumbus/file_generators.rb
@@ -1,14 +1,27 @@
+require 'fileutils'
+
 module Plumbus
   module FileGenerators
     def self.new_project path
+      FileUtils.mkdir_p path
       Dir.chdir path do
-        File.open "Gemfile", "w" do |g|
-          g.puts "source 'https://rubygems.org'"
-          g.puts "gem 'plumbus'"
+        File.open "Gemfile", "w" do |f|
+          f.puts <<HERE
+source 'https://rubygems.org'
+gem 'plumbus'
+HERE
         end
 
-        File.open "routes", "w" do |g|
-          g << "#routes"
+        File.open "routes.rb", "w" do |f|
+          f.puts <<HERE
+port(:request, 'plumbus_http') do
+  #Driver specific config
+end
+
+port(:response, 'plumbus_sockio') do
+  #Driver specific config
+end
+HERE
         end
       end
     end
--- spec/file_generators_spec.rb
+++ spec/file_generators_spec.rb
@@ -1,16 +1,22 @@
 require './spec/spec_helper'
 
 describe Plumbus::FileGenerators do
-  let(:dir) { Dir.mktmpdir }
+  let(:_dir) { Dir.mktmpdir }
+  let(:dir) { _dir }
   subject do 
     Dir["#{dir}/*"].map{|e| Pathname(e).relative_path_from(Pathname(dir)).to_s}
   end
+  before { Plumbus::FileGenerators.new_project dir }
 
   describe '#new_project' do
-    before { Plumbus::FileGenerators.new_project dir }
+    it { should include("routes.rb") }
+    it { should include("Gemfile") }
 
-    it {
-      should include("Gemfile")
-    }
+    context "when dir doesn't exist" do
+      let(:dir) { _dir + '/non_exstant' }
+
+      it { should include("routes.rb") }
+      it { should include("Gemfile") }
+    end
   end
 end
